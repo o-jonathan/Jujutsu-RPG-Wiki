aaf1a8ac490697a9eae36e75249506507e43e858
unknown <jonathanbonatto.pf020@academico.ifsul.edu.br>
2025-12-18 14:30:06 -0300

    18/12 - 14h

--- Wiki/js/main.js
+++ Wiki/js/main.js
@@ -12,7 +12,7 @@ async function carregarDados(tipo) {
     div.classList.add('sheet');
     div.classList.add('hover-up');
     div.innerHTML = `
-      ${el.imagem ? `<img src="${el.imagem}" alt="${el.nome}" onerror="this.onerror=null; this.src='./assets/placeholder.jpg';">` : ''}
+      "<img src="./assets/${el.imagem}" alt="${el.nome}" onerror="this.onerror=null; this.src='./assets/placeholder.jpg';">"
       <h3>${el.nome}</h3>
       ${el.cla ? `<p><strong>Clã:</strong> ${el.cla}</p>` : ''}
       ${el.tecnica ? `<p><strong>Técnica:</strong> ${el.tecnica}</p>` : ''}
@@ -31,11 +31,18 @@ function sbCarregar(dados) {
 
   dados.forEach(el => {
     const div = document.createElement('div');
-    div.classList.add('card');
-    div.classList.add('sheet');
-    div.classList.add('hover-up');
+
+    div.id = `${el.tipo}-${el.id}`;
+    div.classList.add('card', 'sheet', 'hover-up');
+
+    const imagem = el.nome
+    .toLowerCase()
+    .split(' ')[0]
+    .replace('ō', 'o')
+    +'.jpg';
+
     div.innerHTML = `
-      ${el.imagem ? `<img src="./assets/${el.imagem}" alt="${el.nome}" onerror="this.onerror=null; this.src='./assets/placeholder.jpg';">` : ''}
+      <img src="https://brffbxweqoifznvxpgtc.supabase.co/storage/v1/object/public/characters/${imagem}" alt="${el.nome}" onerror="this.onerror=null; this.src='./assets/placeholder.jpg';">
       <h3>${el.nome}</h3>
       ${el.cla ? `<p><strong>Clã:</strong> ${el.cla}</p>` : ''}
       ${el.tecnica ? `<p><strong>Técnica:</strong> ${el.tecnica}</p>` : ''}
